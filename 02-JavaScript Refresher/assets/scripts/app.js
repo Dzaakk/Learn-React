e2836d3a24f6d022f0f3d675adf920688aaf2569
Dzkzzz <dzdzaakk@gmail.com>
2026-04-17 21:25:27 +0700

    functions and parameters

--- 02-JavaScript Refresher/assets/scripts/app.js
+++ 02-JavaScript Refresher/assets/scripts/app.js
@@ -1,11 +1,20 @@
 // import { apikey } from "./util.js";
 // import  apiKey  from "./util.js";
 // import { abc, apiKey } from "./util.js";
-import * as util from "./util.js";
+// import * as util from "./util.js";
 
 // import { abc as content } from "./util.js";
 // console.log(content);
 
-console.log(util.apiKey);
-console.log(util.abc);
-console.log(util.default);
+// console.log(util.apiKey);
+// console.log(util.abc);
+// console.log(util.default);
+
+function greetUser(userName, message = "Hello!") {
+    // console.log(userName);
+    // console.log(message);
+    return "Hi, I am " + userName + ". " + message;
+}
+
+greetUser("Max");
+console.log(greetUser("Bob", "What's up?"));
