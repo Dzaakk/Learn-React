6ad916d9294f372ed0edd673b9534436921c5c62
Dzkzzz <dzdzaakk@gmail.com>
2026-04-17 21:57:05 +0700

    objects and classes

--- 02-JavaScript Refresher/assets/scripts/app.js
+++ 02-JavaScript Refresher/assets/scripts/app.js
@@ -10,16 +10,42 @@
 // console.log(util.abc);
 // console.log(util.default);
 
-function greetUser(userName, message = "Hello!") {
-    // console.log(userName);
-    // console.log(message);
-    return "Hi, I am " + userName + ". " + message;
+// function greetUser(userName, message = "Hello!") {
+//     // console.log(userName);
+//     // console.log(message);
+//     return "Hi, I am " + userName + ". " + message;
+// }
+
+// greetUser("Max");
+// console.log(greetUser("Bob", "What's up?"));
+
+// export default (userName, message) => {
+//     console.log('Hello');
+//     return userName + message;
+// }
+
+const user = {
+    name: "Bob",
+    age: 20,
+    greet() {
+        console.log("Hello!");
+        console.log(this.name);
+    }
 }
 
-greetUser("Max");
-console.log(greetUser("Bob", "What's up?"));
+user.greet();
+console.log(user.age);
 
-export default (userName, message) => {
-    console.log('Hello');
-    return userName + message;
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    greet() {
+        console.log('Hi!');
+    }
 }
+
+const user1 = new User("Minerva", 22);
+console.log(user1);
+user1.greet();
