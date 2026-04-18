3faefc1030188e4b49919a742d9385c473eedb5e
Dzkzzz <dzdzaakk@gmail.com>
2026-04-18 23:15:32 +0700

    using function as a value

--- 02-JavaScript Refresher/assets/scripts/app.js
+++ 02-JavaScript Refresher/assets/scripts/app.js
@@ -126,5 +126,25 @@
 
 // }
 
-const list = document.querySelector("ul")
-list.remove();
+// const list = document.querySelector("ul")
+// list.remove();
+
+function handleTimeout() {
+    console.log("Timed out!");
+}
+
+const handleTimeout2 = () => {
+    console.log("Timed out .. again!");
+}
+
+setTimeout(handleTimeout, 2000)
+setTimeout(handleTimeout2, 3000)
+setTimeout(() => {
+    console.log("More timing out...");
+}, 4000)
+
+function greeter(greetFn) {
+    greetFn();
+}
+
+greeter(() => console.log("Hi"));
