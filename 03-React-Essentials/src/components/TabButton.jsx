b512431afea52c3caaea0ba69a49bd10e174c06d
Dzkzzz <dzdzaakk@gmail.com>
2026-04-21 20:47:03 +0700

    reacting to events

--- 03-React-Essentials/src/components/TabButton.jsx
+++ 03-React-Essentials/src/components/TabButton.jsx
@@ -1,9 +1,13 @@
 // export default function TabButton({ label }) {
 export default function TabButton({ children }) {
+    function handleClick() {
+        console.log('Hello!');
+
+    }
     return (
         <li>
             {/* <button>{label}</button> */}
-            <button>{children}</button>
+            <button onClick={handleClick}>{children}</button>
         </li>
     )
 }
