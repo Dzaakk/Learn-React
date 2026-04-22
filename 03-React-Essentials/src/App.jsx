074ef78662a576684a15b201ce7d0816bb565d4c
Dzkzzz <dzdzaakk@gmail.com>
2026-04-22 20:35:38 +0700

    managing state and using hooks

--- 03-React-Essentials/src/App.jsx
+++ 03-React-Essentials/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import CoreConcept from './components/CoreConcept.jsx';
 import Header from './components/Header/Header.jsx';
 import TabButton from './components/TabButton.jsx';
@@ -5,10 +6,15 @@ import { CORE_CONCEPTS } from './data';
 
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState('Please click a button');
+
   function handleSelect(selectedButton) {
     // selectedButton => 'components', 'jsx', 'props', 'state'
+    setSelectedTopic(selectedButton)
     console.log(selectedButton);
   }
+  console.log('APP COMPONENT EXECUTING');
+
 
   return (
     <div>
@@ -36,7 +42,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          Dynamic Content
+          {selectedTopic}
         </section>
       </main>
     </div>
--- 03-React-Essentials/src/components/TabButton.jsx
+++ 03-React-Essentials/src/components/TabButton.jsx
@@ -1,5 +1,7 @@
 // export default function TabButton({ label }) {
 export default function TabButton({ children, onSelect }) {
+    console.log('TABBUTTON EXECUTING');
+
     return (
         <li>
             {/* <button>{label}</button> */}
