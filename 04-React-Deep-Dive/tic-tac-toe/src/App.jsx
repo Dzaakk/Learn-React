2aebc824e5fe295c8f35324efc41465a3f22b087
Dzkzzz <dzdzaakk@gmail.com>
2026-05-01 00:04:57 +0700

    deriving computed values from other computed values

--- 04-React-Deep-Dive/tic-tac-toe/src/App.jsx
+++ 04-React-Deep-Dive/tic-tac-toe/src/App.jsx
@@ -26,18 +26,27 @@ function App() {
 
   let gameBoard = initialGameBoard;
 
-  for (const turn of turns) {
+  for (const turn of gameTurns) {
     const { square, player } = turn;
     const { row, col } = square;
 
     gameBoard[row][col] = player;
   }
 
-  for (const combination of WINNING_COMBINATIONS) {
-    const firstSquareSymbol
-    const SecondSquareSymbol
-    const thirdSquareSymbol
+  let winner;
 
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
   }
   const activePlayer = deriveActivePlayer(gameTurns)
 
@@ -63,6 +72,7 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
+        {winner && <p>You won, {winner}!</p>}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns}></Log>
